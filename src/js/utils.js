aaf086b504662c8e742f67cb43edae3d3e775489
Cendeal <1798062051@qq.com>
2020-06-26 16:35:20 +0800

    update

--- src/js/utils.js
+++ src/js/utils.js
@@ -5,14 +5,12 @@ function showMenu() {
             if (navElement.hidden) {
                 navElement.hidden = false
                 navElement.expend = false
-                // navElement.style.marginTop='';
-                // navElement.style.zIndex='';
-                navElement.style.transform='scaleY(0)'
+                navElement.style.transform = 'scaleY(0)'
                 navElement.style.position = ''
             } else {
                 navElement.hidden = true
                 navElement.expend = true
-                navElement.style.transform='scaleY(1)'
+                navElement.style.transform = 'scaleY(1)'
                 navElement.style.position = 'static'
 
             }
@@ -22,11 +20,7 @@ function showMenu() {
 
 function resizeListen() {
     window.onresize = function () {
-        document.querySelectorAll("*").forEach(item => {
-            if (item.expend) {
-                item.style.display = ""
-            }
-        })
+      window.location.reload()
     };
 }
 
@@ -44,21 +38,23 @@ function scrollListen() {
 
     }
 }
-function openType(type){
+
+function openType(type) {
     switch (type) {
         case 'wechat':
             alert(type)
             break
         case 'github':
-            window.open('https://github.com/Cendeal','__blank')
+            window.open('https://github.com/Cendeal', '__blank')
             break
         case 'gitee':
-            window.open('https://gitee.com/Cendeal','__blank')
+            window.open('https://gitee.com/Cendeal', '__blank')
             break
         case 'email':
             window.open('mailto:1798062051@qq.com')
             break
     }
 }
+
 scrollListen()
 resizeListen()
